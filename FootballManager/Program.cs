f69801c4021cf703e5e161dfa4346345e955178d
Patrik <patrik.odh@gmail.com>
2022-07-13 19:49:10 +0200

    test

--- FootballManager/Program.cs
+++ FootballManager/Program.cs
@@ -6,27 +6,42 @@ List<string> firstRow = new List<string>();
 using (SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=FootballManager;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"))
 {
     con.Open();
-    using(StreamReader reader = new StreamReader(@"C:\Users\patri\Desktop\LIA-uppgift\Premier_league-2017-2018.csv"))
+
+
+    using (StreamReader reader = new StreamReader(@"C:\Users\patri\Desktop\LIA - Uppgift\Premier_league-2017-2018.csv"))
     {
+        int counter = 0;
         while (!reader.EndOfStream)
         {
             var line = reader.ReadLine();
-            var values = line.Split(';',',');
-            firstRow.Add(values[2].ToString());
-            //var query = "insert into blblb";
-            //using(var command = new SqlCommand(query, con))
-            //{
-            //    command.ExecuteNonQuery();
-            //}
+            var values = line.Split(';', ',');
+            if (counter > 0)
+            {
+                firstRow.Add(values[2].ToString());
+                firstRow.Add(values[3].ToString());
+            }
+            counter++;
         }
+        var result = firstRow.Distinct().ToList();
+
+        foreach (var item in result)
+        {
+            var query = $"insert into Teams(TeamName) values('{item}')";
+            using (var command = new SqlCommand(query, con))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+ 
     }
     con.Close();
 }
 
-var result = firstRow.Distinct().ToList();
 
 
-foreach (var item in result)
-{
-    Console.WriteLine(item);
-}
+//foreach (var iem in result)
+//{
+//    Console.WriteLine(item);
+//}
